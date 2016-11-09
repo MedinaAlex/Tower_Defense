0e40f7688f00730447ba5862c82a20bc0e736530
Jacques Lesur <lesurjacques@gmail.com>
2016-11-09 11:00:28 +0100

    Class

---
+++ tower-defense/src/main/Case.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+
+public class Case {
+    boolean chemin = false;
+    ArrayList listEnemis = new ArrayList<Ennemi>();
+    Tour tour = null;
+    boolean depart = false;
+    boolean arrive = false;
+
+    public void Case (boolean chemin, boolean depart, boolean arrive){
+        this.chemin = chemin;
+        this.depart = depart;
+        this.arrive = arrive;
+    }
+}
---
+++ tower-defense/src/main/Ennemi.java
@@ -0,0 +1,17 @@
+/**
+ * Created by Jacques on 09/11/2016.
+ */
+public class Ennemi {
+    String nom;
+    int vitesse;
+    int pv;
+    int degat;
+    int prix;
+    public void Ennemi (String nom, int vitesse, int pv, int degat, int prix){
+        this.nom = nom;
+        this.vitesse = vitesse;
+        this.pv = pv;
+        this.degat = degat;
+        this.prix = prix;
+    }
+}
