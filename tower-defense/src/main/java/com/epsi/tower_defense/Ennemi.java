7ef5f0310d8dd86e5d2e23d2bf9e7b2950087fa8
Alex Medina <alex.medina@hotmail.fr>
2016-12-08 09:14:35 +0100

    implementation

--- tower-defense/src/main/java/com/epsi/tower_defense/Ennemi.java
+++ tower-defense/src/main/java/com/epsi/tower_defense/Ennemi.java
@@ -8,12 +8,29 @@ public class Ennemi {
     int pv;
     int degat;
     int prix;
+    int[] ancienneCoordonées;
+    int[] actuelleCoordonées;
+   
     
-    public Ennemi (String nom, int vitesse, int pv, int degat, int prix){
+    public Ennemi (String nom, int vitesse, int pv, int degat, int prix, int[] actuelle){
         this.nom = nom;
         this.vitesse = vitesse;
         this.pv = pv;
         this.degat = degat;
         this.prix = prix;
+        this.actuelleCoordonées = actuelle;
     }
+    
+    public void deplacement(){
+    	this.ancienneCoordonées = this.actuelleCoordonées;
+    	// TODO
+    }
+    
+    public void perdrePV(int degat){
+    	this.pv -= degat;
+    }
+    
+    public boolean verifierEnVie(){
+    	return pv <= 0;
+    }    
 }
